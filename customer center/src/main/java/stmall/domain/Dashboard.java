e031d469b1ded03971d02055b28a7c1ff302003d
Hayoung Song <merrycoral@gmail.com>
2024-10-22 15:33:27 +0900

    commit

---
+++ customer center/src/main/java/stmall/domain/Dashboard.java
@@ -0,0 +1,24 @@
+package stmall.domain;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+
+//<<< EDA / CQRS
+@Entity
+@Table(name = "Dashboard_table")
+@Data
+public class Dashboard {
+
+    @Id
+    //@GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    private Long orderId;
+    private String userId;
+    private String productId;
+    private String qty;
+    private String status;
+}
---
+++ delivery/src/main/java/stmall/domain/OrderPlaced.java
@@ -0,0 +1,19 @@
+package stmall.domain;
+
+import java.util.*;
+import lombok.*;
+import stmall.domain.*;
+import stmall.infra.AbstractEvent;
+
+@Data
+@ToString
+public class OrderPlaced extends AbstractEvent {
+
+    private Long id;
+    private String userId;
+    private String productName;
+    private String productId;
+    private String qty;
+    private String status;
+    private String address;
+}
